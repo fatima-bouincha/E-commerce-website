3f8edabce05a3159ada3902453af3cf6bc7ca531
fatima-bouincha <fatimabouincha@gmail.com>
2026-05-07 14:23:13 +0100

    first commit

---
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import navlogo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
+import './navbar.css'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={navlogo} alt="" className="nav-logo" />
+        <img src={navProfile} className='nav-profile' alt="" />
+    </div>
+  )
+}
+
+export default Navbar
