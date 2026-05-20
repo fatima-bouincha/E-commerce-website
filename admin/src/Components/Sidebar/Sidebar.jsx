4a60969588b173f713e566c626d87353cc83d56a
fatima-bouincha <fatimabouincha@gmail.com>
2026-05-20 14:26:22 +0100

    final commit

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
         </Link>
 
 
-        <Link to={'/list_product'} style={{textDecoration:"none"}}>
+        <Link to={'/listproduct'} style={{textDecoration:"none"}}>
         <div className="sidebar-item">
             <img src={list_product_icon} alt="" />
             <p>Product List</p>
